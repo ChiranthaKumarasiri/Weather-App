2b2cc4249ed402841deca8091e1a42ea0d5c718f
ChiranthaKumarasiri <chiranthakumarasiri21@gmail.com>
2025-05-22 23:45:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request
 import requests
+import os
 
 app = Flask(__name__)
 
-API_KEY = "c97431f904476442d7cad51c78e56528"  
+API_KEY = "c97431f904476442d7cad51c78e56528"
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -25,4 +26,5 @@ def index():
     return render_template("index.html", weather=weather)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
